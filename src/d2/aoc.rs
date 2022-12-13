92ce8b10c05c11ef28b21d468d80693b801bc7a6
blennster <emil.blennow99@gmail.com>
2022-12-13 12:19:06 +0100

    cleanup d2

--- src/d2/aoc.rs
+++ src/d2/aoc.rs
@@ -1,3 +1,5 @@
+use std::str::FromStr;
+
 #[derive(Clone, Copy, PartialEq)]
 enum Move {
     Rock = 1,
@@ -5,16 +7,19 @@ enum Move {
     Scissors = 3,
 }
 
-impl Move {
-    fn from_str(c: &str) -> Self {
-        match c {
-            "A" | "X" => Move::Rock,
-            "B" | "Y" => Move::Paper,
-            "C" | "Z" => Move::Scissors,
-            _ => panic!("Invalid move"),
+impl FromStr for Move {
+    type Err = String;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "A" | "X" => Ok(Move::Rock),
+            "B" | "Y" => Ok(Move::Paper),
+            "C" | "Z" => Ok(Move::Scissors),
+            _ => Err("unknown move".to_string()),
         }
     }
+}
 
+impl Move {
     fn lose_to(&self) -> Self {
         match self {
             Move::Rock => Move::Paper,
@@ -50,8 +55,8 @@ enum Strategy {
     Lose,
 }
 
-impl Strategy {
-    fn from_move(m: &Move) -> Self {
+impl From<Move> for Strategy {
+    fn from(m: Move) -> Self {
         match m {
             // X
             Move::Rock => Strategy::Lose,
@@ -61,20 +66,14 @@ impl Strategy {
             Move::Scissors => Strategy::Win,
         }
     }
+}
 
+impl Strategy {
     fn get_move(&self, opponent: &Move) -> Move {
         match self {
             Strategy::Win => opponent.lose_to(),
             Strategy::Draw => *opponent,
             Strategy::Lose => opponent.win_to(),
-        }
-    }
-
-    fn to_outcome(&self) -> Outcome {
-        match self {
-            Strategy::Win => Outcome::Win,
-            Strategy::Draw => Outcome::Draw,
-            Strategy::Lose => Outcome::Lose,
         }
     }
 }
@@ -85,19 +84,33 @@ enum Outcome {
     Lose = 0,
 }
 
-pub fn aoc(lines: &[String]) {
-    let moves: Vec<Vec<Move>> = lines
-        .iter()
-        .map(|line| line.split(' ').map(Move::from_str).collect())
-        .collect();
-
-    p1(&moves);
-    p2(&moves);
+impl From<Strategy> for Outcome {
+    fn from(strategy: Strategy) -> Self {
+        match strategy {
+            Strategy::Win => Outcome::Win,
+            Strategy::Draw => Outcome::Draw,
+            Strategy::Lose => Outcome::Lose,
+        }
+    }
 }
 
-fn p1(moves: &[Vec<Move>]) {
+pub fn aoc(lines: &[String]) {
+    let rounds: Vec<Vec<Move>> = lines
+        .iter()
+        .map(|line| {
+            line.split(' ')
+                .map(|x| Move::from_str(x).unwrap())
+                .collect()
+        })
+        .collect();
+
+    p1(&rounds);
+    p2(&rounds);
+}
+
+fn p1(rounds: &[Vec<Move>]) {
     let mut score = 0;
-    for round in moves {
+    for round in rounds {
         let [opponent, player] = [round[0], round[1]];
         let outcome = player.get_outcome(&opponent);
         score += outcome as i32 + player as i32;
@@ -106,13 +119,13 @@ fn p1(moves: &[Vec<Move>]) {
     println!("{}", score);
 }
 
-fn p2(moves: &[Vec<Move>]) {
+fn p2(rounds: &[Vec<Move>]) {
     let mut score = 0;
-    for round in moves {
-        let [opponent, field2] = [round[0], round[1]];
-        let strategy = Strategy::from_move(&field2);
-        let outcome = strategy.to_outcome();
+    for round in rounds {
+        let opponent = round[0];
+        let strategy: Strategy = round[1].into();
         let m = strategy.get_move(&opponent);
+        let outcome: Outcome = strategy.into();
 
         score += outcome as i32 + m as i32;
     }
